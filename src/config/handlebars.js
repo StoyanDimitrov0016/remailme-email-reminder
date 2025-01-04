ec3aedc646b7e771309868772e05e4fc66e4015f
StoyanDimitrov0016 <stoyan1020@gmail.com>
2025-01-04 22:32:53 +0200

    feat: add handlebars configuration

---
+++ src/config/handlebars.js
@@ -0,0 +1,19 @@
+import { create } from "express-handlebars";
+import path from "path";
+
+const hbs = create({
+  defaultLayout: "main",
+  layoutsDir: path.join("src", "views", "layouts"),
+  partialsDir: path.join("src", "views", "partials"),
+  helpers: {
+    currentYear: () => new Date().getFullYear(),
+  },
+});
+
+const setupHandlebars = (app) => {
+  app.engine("handlebars", hbs.engine);
+  app.set("view engine", "handlebars");
+  app.set("views", path.resolve("src", "views"));
+};
+
+export default setupHandlebars;
